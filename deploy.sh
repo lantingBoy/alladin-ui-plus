2b680eedf36b8a8366df095a9515301765f9e350
zhoumengxiang <mengxiang.zhou.h5@gmail.com>
2024-09-27 10:37:44 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,10 @@ npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vitepress/dist
-
-git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/lantingBoy/alladin-ui-plus.git master:gh-pages
 
-cd -
+cd ..
--- packages/proSearch/src/TableActions/index.vue
+++ packages/proSearch/src/TableActions/index.vue
@@ -11,12 +11,7 @@
 	<div class="table_actions">
 		<div class="config-header" v-if="tableLength || showAll">
 			<div class="tools-bar">
-				<el-button link v-print="printInfo" v-if="printAreaId">
-					<template #icon>
-						<i class="hdzkIcon icon-dayin"></i>
-					</template>
-					打印
-				</el-button>
+				
 				<el-dropdown trigger="click" style="margin-left: 20px" v-if="showActions.actions">
 					<span class="dropdown-link">
 						<i class="hdzkIcon icon-dongzuo" style="vertical-align: middle"></i>
@@ -37,38 +32,10 @@
 <script setup lang="ts"  name="TableActions">
 import { ref } from 'vue';
 import { OptionsIsShow } from '../types';
-import print from 'vue3-print-nb';
 
-const vPrint = print;
 const props = defineProps<{ printAreaId?: string; tableLength: number; showActions: OptionsIsShow; showAll: boolean }>();
 //打印
-const printInfo = ref({
-	id: props.printAreaId, //这里的id就是上面我们的打印区域id，实现指哪打哪
-	popTitle: '', // 打印配置页上方的标题
-	extraHead: '', // 最上方的头部文字，附加在head标签上的额外标签，使用逗号分割
-	preview: false, // 是否启动预览模式，默认是false
 
-	previewPrintBtnLabel: '预览结束，开始打印', // 打印预览的标题下方的按钮文本，点击可进入打印
-	zIndex: 20003, // 预览窗口的z-index，默认是20002，最好比默认值更高
-	previewBeforeOpenCallback() {
-		console.log('正在加载预览窗口！');
-	}, // 预览窗口打开之前的callback
-	previewOpenCallback() {
-		console.log('已经加载完预览窗口，预览打开了！');
-	}, // 预览窗口打开时的callback
-	beforeOpenCallback() {
-		console.log('开始打印之前！');
-	}, // 开始打印之前的callback
-	openCallback() {
-		console.log('执行打印了！');
-	}, // 调用打印时的callback
-	closeCallback() {
-		console.log('关闭了打印工具！');
-	}, // 关闭打印的callback(无法区分确认or取消)
-	clickMounted() {
-		console.log('点击v-print绑定的按钮了！');
-	}
-});
 </script>
 
 <style lang="scss" scoped>
